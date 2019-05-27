4e77b2791f855b71d2ac077aa329cebe15503872
Justin Kenney <justinkenney13@gmail.com>
2019-05-27 20:40:28 +0000

    Automatically backed up by Learn

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
-  
+
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
       return <div key={idx} onClick={() => this.listener(str)} className="color-swatch" style={{backgroundColor: str}}/>
@@ -11,7 +11,7 @@ export default class ColorSelector extends Component {
   listener = (color) => {
     this.props.selectColor(color)
   }
-  
+
   render() {
     return (
       <div id="colorSelector">
@@ -19,5 +19,5 @@ export default class ColorSelector extends Component {
       </div>
     )
   }
-  
+
 }
